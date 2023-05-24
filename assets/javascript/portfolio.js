839b66a004030caa72b50d500939c355c509a5a4
Revertz <113061833+imrevertz@users.noreply.github.com>
2023-05-24 09:46:17 -0400

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,61 +1,59 @@
-/*
-
-Credits:
-    - Lummit - https://obnoxious.club/ | https://github.com/Lumm1t/ | Discord: Lummit#0201
-    - expl0it, shellcode.team
-    - Steam (emoticon: https://steamcommunity-a.akamaihd.net/economy/emoticon/mgh_17)
-    - Trollface image (http://www.rw-designer.com/icon-image/7835-256x256x32.png)
-    - Tumblr images/icons:
-        a) https://68.media.tumblr.com/730ba51e7f6b0203e023deeb0db8367b/tumblr_osgbbiLXWV1suieauo1_500.jpg
-        b) https://68.media.tumblr.com/8436227895295acecdea170b612fb766/tumblr_nxgg5bYqm81rclv0wo1_500.jpg
-        c) http://images.neon-sign.org/l-m/rose-flower-love-neon-sign.jpg
-        d) https://media.istockphoto.com/vectors/rose-neon-sign-vector-id1088857104?k=6&m=1088857104&s=612x612&w=0&h=ZJCYow5i8noF6nTGHEd6b2bbe9X6rVx4cLDoceexAec=
-        e) https://image.freepik.com/free-vector/polygonal-geometric-rose-neon-sign_1262-19626.jpg
-        f) https://68.media.tumblr.com/49394d5aa66d4c7ea4ffebda38c6be7e/tumblr_orv2rd9eVC1rgewhto1_400.gif
-        g) https://ih1.redbubble.net/image.268210271.5200/sticker,375x360-bg,ffffff.u2.png
-        h) http://picture-cdn.wheretoget.it/ot63b4-l-610x610-shirt-rose-tumblr-grunge-grunge+t+shirt-clothes-t+shirt-tumblr+shirt-dead+roses-black+bear-soft+grunge-soft-soft+ghetto-ghetto-chill-cute-white-red-red+roses-white+tee-pale-sad-dar.jpg
-
-Thanks for:
-    - Google
-    - StackOverflow
-    - jQuery
-    - jQuery Marquee
-    - animate.css
-    - typed.js
-
-GitHub: https://github.com/Lumm1t/obnoxious.club
-
-*/
+'use strict';
 
 'use strict';
 
-const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=1785ed53312f42c7b5ef89f65c3faa1a';
+const ipgeolocation = 'https://api.ipgeolocation.io/ipgeo?apiKey=813ff81068c84cf7980735c716e89e09';
 
 const timeouts = [];
 
 const mobileAndTabletCheck = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
+
 $(document).ready(() => {
   const links = [
     {
-      name: 'Lummit',
-      link: '76561197960276740',
+      name: 'bazzy',
+      link: '76561197960958773',
     },
     {
-      name: 'Neso',
-      link: '76561198193201687',
+      name: 'sean',
+      link: '76561198121800913',
     },
     {
-      name: 'loljuxd',
-      link: '76561198016198032',
+      name: 'devvo',
+      link: '76561198881137975',
     },
     {
-      name: 'Lost',
-      link: '76561199079530557',
+      name: 'unknown-ct',
+      link: '76561198063247994',
     },
     {
-      name: 'mannin',
-      link: '76561197975672336',
+      name: '3cho',
+      link: '76561198173548590'
+    },
+    {
+      name: 'moe',
+      link: '76561198364985687'
+    },
+    {
+      name: 'trappo',
+      link: '76561197961058952',
+    },
+    {
+      name: 'angel',
+      link: '76561198893938524',
+    },
+    {
+      name: 'adxm',
+      link: '76561198962972012',
+    },
+    {
+      name: 'connor',
+      link: '76561199188488467',
+    },
+    {
+      name: 'archie',
+      link: '76561198392193931',
     },
   ];
 
@@ -66,7 +64,7 @@ $(document).ready(() => {
 
     link = $('#marquee').children('a').last();
 
-    if (i != links.length - 1) $('#marquee').append(' <img class="emoticon" src="assets/others/mgh_17.png"> ');
+    if (i != links.length - 1) $('#marquee').append(' <img class="emoticon" src="assets/icons/rose1.png"> ');
   }
 
   if (mobileAndTabletCheck()) {
@@ -75,8 +73,8 @@ $(document).ready(() => {
     app.shouldIgnoreVideo = true;
   }
 
-  app.titleChanger(['Lummit', 'Neso', 'loljuxd', 'Vez', 'mannin', '=', 'obnoxious.club', 'Song name: KUSO GVKI - Cinnamon (feat. Apfel) (splash! Mag Premiere)']);
-  app.iconChanger(['assets/icons/roses/rose1.jpg', 'assets/icons/roses/rose2.jpg', 'assets/icons/roses/rose3.jpg', 'assets/icons/roses/rose4.jpg', 'assets/icons/roses/rose5.jpg', 'assets/icons/roses/rose6.jpg', 'assets/icons/roses/rose7.jpg', 'assets/icons/roses/rose8.jpg', 'assets/icons/roses/rose1.jpg']);
+  app.titleChanger(['shoreline.rip', 'hi infistar', 'hi fini', 'arma 3 terrorists', 'shoreline.rip', '=', 'gods', 'Song name: All The Time - Shoreline Mafia']);
+  app.iconChanger(['assets/icons/roses/rose1.png']);
 });
 
 if ($.cookie('videoTime')) {
@@ -107,10 +105,10 @@ $('html').on('contextmenu', (event) => {
 
   const trollfaceLight = app.skippedIntro ? '' : 'trollface-light';
 
-  img.src = 'assets/others/trollface.jpg';
+  img.src = 'assets/icons/rose1.png';
   img.width = 64;
   img.height = 64;
-  img.alt = 'obnoxious.club';
+  img.alt = 'shoreline.rip';
   img.style = `position: absolute; left: ${event.pageX}px; top: ${event.pageY}px; z-index: 10`;
   img.className = `troll ${trollfaceLight}`;
 
@@ -119,7 +117,7 @@ $('html').on('contextmenu', (event) => {
 
 setInterval(() => {
   $('.troll').remove();
-}, 600);
+}, 200);
 
 $('.skip').click(() => {
   skipIntro();
@@ -152,102 +150,106 @@ const writeLine = (text, speed, timeout, callback) => {
 };
 
 $.getJSON(ipgeolocation, (data) => {
-  writeLine(['Authenticating...', "Granting access to <span style='font-size: 14px; color: #06d;'>[unknown]</span>..."], 30, () => {
-    if (app.skippedIntro) return;
+  writeLine(["Authenticating...", "Granting access to <span style='font-size: 14px; color: #06d;'>[shoreline.rip]</span>..."], 30, function () {
+
+    if (app.skippedIntro)
+      return;
 
     clearCursor();
 
-    const usernames = ['user', 'dude'];
+      var usernames = ["user", "dude"];
 
-    const ip = data.ip ? data.ip : usernames[Math.floor(Math.random() * usernames.length)];
-    const country = data.country_name ? data.country_name : 'your country';
+      writeLine(["Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>", "Welcome back, <i style='color: #0f0'>" +  ((data.ip) ? data.ip : usernames[Math.floor(Math.random()*usernames.length)]) 
+          + "</i>! By the way, nice to see someone from " + ((data.country_name) ? data.country_name : 'your country') + " here!"], 30, 500, function () {
 
-    writeLine([`Access granted! <span style='font-size: 14px; color: #0f0;'>[success]</span>`, `Welcome back, <i style='color: #0f0'>${ip}</i>! By the way, nice to see someone from ${country} here!`], 30, 500, () => {
-      if (app.skippedIntro) return;
+          if (app.skippedIntro)
+              return;
 
-      clearCursor();
-
-      writeLine([`<i style='color: #F62459'>obnoxious.club $$$</i>`], 120, 500, () => {
-        timeouts.push(
-          setTimeout(() => {
-            if (app.skippedIntro) return;
-
-            clearCursor();
-
-            setTimeout(() => {
-              skipIntro();
-            }, 500);
-          }, 1000)
-        );
-      });
-    });
-  });
-});
-
-const skipIntro = () => {
-  if (app.skippedIntro) return;
-
-  app.skippedIntro = true;
-
-  timeouts.forEach((timeout) => {
-    clearTimeout(timeout);
-  });
-
-  $('.top-right').remove();
-
-  $('#main').fadeOut(100, () => {
-    $('#main').remove();
-
-    $('#marquee').marquee({
-      duration: 15000,
-      gap: 420,
-      delayBeforeStart: 1000,
-      direction: 'left',
-      duplicated: true,
-    });
-
-    setTimeout(() => {
-      $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
-    }, 200);
-
-    setTimeout(() => {
-      const typed = new Typed('#brand', {
-        strings: app.brandDescription,
-        typeSpeed: 40,
-
-        onComplete: () => {
           clearCursor();
-        },
+
+          writeLine(["<i style='color: #F62459'>shoreline.rip $$$</i>"], 120, 500, function () {
+
+            timeouts.push(
+              setTimeout(() => {
+                if (app.skippedIntro) return;
+    
+                clearCursor();
+    
+                setTimeout(() => {
+                  skipIntro();
+                }, 500);
+              }, 1000)
+            );
+          });
+        });
       });
-    }, 1350);
-
-    setTimeout(() => {
-      if (!app.shouldIgnoreVideo) {
-        app.videoElement.play();
-        app.audioElement.play();
-      }
-
-      app.videoElement.addEventListener(
-        'timeupdate',
-        () => {
-          $.cookie('videoTime', app.videoElement.currentTime, { expires: 1 });
-        },
-        false
-      );
-
-      $('.marquee-container').css('visibility', 'visible').hide().fadeIn(100);
-
-      $('.marquee-container').animateCss('zoomIn');
-
-      $('.container').fadeIn();
-
-      $('.background').fadeIn(200, () => {
-        if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
+    });
+    
+    const skipIntro = () => {
+      if (app.skippedIntro) return;
+    
+      app.skippedIntro = true;
+    
+      timeouts.forEach((timeout) => {
+        clearTimeout(timeout);
       });
-    }, 200);
-  });
-};
-
-const clearCursor = () => {
-  return $('span').siblings('.typed-cursor').css('opacity', '0');
-};
+    
+      $('.top-right').remove();
+    
+      $('#main').fadeOut(100, () => {
+        $('#main').remove();
+    
+        $('#marquee').marquee({
+          duration: 15000,
+          gap: 420,
+          delayBeforeStart: 1000,
+          direction: 'left',
+          duplicated: true,
+        });
+    
+        setTimeout(() => {
+          $('.brand-header').animateCss(app.effects[Math.floor(Math.random() * app.effects.length)]);
+        }, 200);
+    
+        setTimeout(() => {
+          const typed = new Typed('#brand', {
+            strings: app.brandDescription,
+            typeSpeed: 40,
+    
+            onComplete: () => {
+              clearCursor();
+            
+            },
+          });
+        }, 1350);
+    
+        setTimeout(() => {
+          if (!app.shouldIgnoreVideo) {
+            app.videoElement.play();
+            app.audioElement.play();
+          }
+    
+          app.videoElement.addEventListener(
+            'timeupdate',
+            () => {
+              $.cookie('videoTime', app.videoElement.currentTime, { expires: 1 });
+            },
+            false
+          );
+    
+          $('.marquee-container').css('visibility', 'visible').hide().fadeIn(100);
+    
+          $('.marquee-container').animateCss('zoomIn');
+    
+          $('.container').fadeIn();
+    
+          $('.background').fadeIn(200, () => {
+            if (!app.shouldIgnoreVideo) $('#audio').animate({ volume: app.musicVolume }, app.musicFadeIn);
+          });
+        }, 200);
+      });
+    };
+    
+    const clearCursor = () => {
+      return $('span').siblings('.typed-cursor').css('opacity', '0');
+    };
